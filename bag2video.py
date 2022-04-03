fa0b88c8019e6be3bce20e3ee8635ad780671843
plucky <zhy674744560@gmail.com>
2022-04-03 16:25:24 +0900

    first version of bag2video

---
+++ bag2video.py
@@ -0,0 +1,124 @@
+import numpy as np
+import pyrealsense2 as rs
+import cv2
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+class Bag2video:
+    """
+    a simply class to transfor bag file to video or image file.
+    address - bag file address
+    """
+    def __init__(self, address):
+        self.__address = address
+        self.__allowed = [rs.stream.color, rs.stream.depth, rs.stream.infrared]
+
+    @staticmethod
+    def get_idx(i, pre_zero=10):
+        first = str(i)
+        return (pre_zero - len(first)) * "0" + first
+
+    @staticmethod
+    def check_file(path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def _bag2trans(self, path, data_stream=rs.stream.color, data_format=rs.format.rgb8, fps=30, pro_type=0, video=None):
+        if data_stream not in self.__allowed:
+            logger.warning("this function can only process [color|depth|infrared] data stream")
+            return
+
+        if pro_type == 0:
+            original = path
+        else:
+            pre_add, video_f = os.path.split(path)
+            original = pre_add
+
+        self.check_file(original)
+
+        # Create pipeline
+        pipeline = rs.pipeline()
+        try:
+            # Create a config object
+            config = rs.config()
+
+            # Tell config that we will use a recorded device from file to be used by the pipeline through playback.
+            rs.config.enable_device_from_file(config, self.__address, repeat_playback=False)
+
+            # Configure the pipeline to stream the data stream
+            # Change this parameters according to the recorded bag file resolution
+            config.enable_stream(data_stream, data_format, fps)
+
+            # Start streaming from file
+            pipeline.start(config)
+
+            # Streaming loop
+            i = 1
+
+            while True:
+                # Get frame set of data stream
+                frames = pipeline.wait_for_frames()
+
+                # Get data frame
+                if data_stream == rs.stream.color:
+                    color_frame = frames.get_color_frame()
+                    color_frame_image = np.array(color_frame.get_data())
+                    data_frame_image = cv2.cvtColor(color_frame_image, cv2.COLOR_RGB2BGR)
+                elif data_stream == rs.stream.depth:
+                    depth_frame = frames.get_depth_frame()
+                    data_frame_image = np.array(depth_frame.get_data())
+                else:
+                    infrared_frame = frames.get_infrared_frame()
+                    data_frame_image = np.array(infrared_frame.get_data())
+                if pro_type == 0:
+                    n_path = os.path.join(original, self.get_idx(i) + ".jpg")
+                    cv2.imwrite(n_path, data_frame_image)
+                else:
+                    video.write(data_frame_image)
+                i += 1
+        except RuntimeError:
+            pass
+        finally:
+            pipeline.stop()
+            if video:
+                video.release()
+
+    def bag2image(self, path, data_stream=rs.stream.color, data_format=rs.format.rgb8, fps=30):
+        self._bag2trans(path, data_stream, data_format, fps)
+
+    def bag2video(self, path, data_stream=rs.stream.color, data_format=rs.format.rgb8, fps=30, size=(1280, 720)):
+        pre_add, video_f = os.path.split(path)
+        file_extension = video_f.split(".")[-1]
+        self.check_file(pre_add)
+        iscolor = True if data_stream == rs.stream.color else False
+        if file_extension == "mp4":
+            video = cv2.VideoWriter(path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), fps, size, iscolor)
+        elif file_extension == "avi":
+            video = cv2.VideoWriter(path, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, size, iscolor)
+        else:
+            logger.warning("I haven't decided on this part")
+            return
+        self._bag2trans(path, data_stream, data_format, fps, 1, video)
+
+    def image2video(self, img_path, video_path, size, fps=30):
+        pre_add, video_f = os.path.split(video_path)
+        file_extension = video_f.split(".")[-1]
+        self.check_file(pre_add)
+        if file_extension == "mp4":
+            video = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), fps, size)
+        elif file_extension == "avi":
+            video = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, size)
+        else:
+            logger.warning("I haven't decided on this part")
+            return
+        file_list = os.listdir(img_path)
+        for item in file_list:
+            if item.endswith('.jpg'):
+                item = os.path.join(img_path, item)
+                img = cv2.imread(item)
+                video.write(img)
+        video.release()
---
+++ test.py
@@ -0,0 +1,30 @@
+from bag2video import Bag2video
+import os
+import pyrealsense2 as rs
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="a method to convert realsense bag file to [image|video] data")
+    # Add argument which takes path to a bag file as an input
+    parser.add_argument("-i", "--input", type=str, help="Path to the bag file", required=True)
+    parser.add_argument("-o", "--output", type=str, help="Path to the output file. e.g c:data/images for images"
+                                                         "c:/data/output.mp4 for video", required=True)
+    parser.add_argument("-s", "--stream", type=str, help="the stream you want to process", default=rs.stream.color)
+    parser.add_argument("-f", "--format", type=str, help="The format corresponding to the data stream",
+                        default=rs.format.rgb8)
+    parser.add_argument("--fps", type=str, help="bag file frame fps", default=30)
+    parser.add_argument("--size", type=str, help="the output video size, only when you want to output video",
+                        default=(1280, 720))
+    # Parse the command line arguments to an object
+    args = parser.parse_args()
+
+    # Check if the given file have bag extension
+    if os.path.splitext(args.input)[1] != ".bag":
+        print("The given file is not of correct file format.")
+        print("Only .bag files are accepted")
+        exit()
+    if os.path.isdir(args.output):
+        Bag2video(args.input).bag2image(args.output, args.stream, args.format, args.fps)
+    else:
+        Bag2video(args.input).bag2video(args.output, args.stream, args.format, args.fps, args.size)
+
